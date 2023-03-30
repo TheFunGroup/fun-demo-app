ab5c663b834b1d61a78d1d3d2307347b0023fe5e
aaronchewbani <aaronchewbani@gmail.com>
2023-03-30 16:50:12 -0700

    approve fix

--- src/scripts/approve.js
+++ src/scripts/approve.js
@@ -2,7 +2,9 @@ import { ethers } from "ethers";
 
 //Amount is in USDC
 export const handleApprove = async function (wallet, auth, paymasterAddress, paymentAddr, amount=500) {
-  
+  await configureEnvironment({
+    gasSponsor: false
+  })
   await wallet.approve(auth, { spender: paymasterAddress, token: paymentAddr, amount: amount })
 
   return { success: true }
