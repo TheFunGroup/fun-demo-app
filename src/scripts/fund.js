d37b83e582b4976152c5482a20fdb5fad4c2890b
chaz <chaz@chazs-MacBook-Pro.local>
2023-04-28 19:11:46 -0700

    update api gateway

--- src/scripts/fund.js
+++ src/scripts/fund.js
@@ -1,4 +1,4 @@
-const FAUCETURL = "https://vyhjm494l3.execute-api.us-west-2.amazonaws.com/prod/demo-faucet/get-faucet"
+const FAUCETURL = "https://kjj7i5hi79.execute-api.us-west-2.amazonaws.com/prod/demo-faucet/get-faucet"
 export const handleFundWallet = async function (addr) {
     try {
         await fetch(`${FAUCETURL}?token=eth&testnet=goerli&addr=${addr}`)
