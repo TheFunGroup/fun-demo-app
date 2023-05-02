47aa98873d660d26135c260061d2bf1e47b4b0d3
aaronchewbani <aaronchewbani@gmail.com>
2023-05-01 18:26:13 -0700

    fix fund wallet

--- src/scripts/wallet.js
+++ src/scripts/wallet.js
@@ -2,7 +2,7 @@ import { FunWallet, configureEnvironment } from "fun-wallet";
 import { getStoredUniqueId } from "fun-wallet/utils";
 import { ethers } from "ethers";
 import { apiKey } from "../utils/constants";
-
+import {handleFundWallet} from '../scripts/fund'
 const options = {
   chain: 5,
   apiKey
@@ -48,11 +48,7 @@ export const isAuthIdUsed = async (authId) => {
 export async function useFaucet(addr, network) {
   if (network == 5) { //GOERLI
     try {
-      await fetch(`http://18.237.113.42:8001/stake-token?testnet=goerli&addr=${addr}`)
-      await fetch(`http://18.237.113.42:8001/get-faucet?token=eth&testnet=goerli&addr=${addr}`)
-      await fetch(`http://18.237.113.42:8001/get-faucet?token=usdc&testnet=goerli&addr=${addr}`)
-      await fetch(`http://18.237.113.42:8001/get-faucet?token=dai&testnet=goerli&addr=${addr}`)
-      await fetch(`http://18.237.113.42:8001/get-faucet?token=usdt&testnet=goerli&addr=${addr}`)
+      await handleFundWallet(addr)
       setTimeout(() => {
         return
       }, 1500)
