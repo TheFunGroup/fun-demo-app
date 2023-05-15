af8042a7835bdac61084f698902b40d6dfe5fddb
Nighttrek <daniels@dual4t.com>
2023-05-14 22:27:40 -0700

    updated the input function to support different labeling and sizing

--- src/components/forms/Input.js
+++ src/components/forms/Input.js
@@ -13,7 +13,11 @@ export default function Input(props) {
         className={`${active ? "border-[#2D4EA2] input-shadow" : "border-[#D0D5DD]"} border-[1px] w-full flex items-center justify-between px-[14px] py-[10px] rounded-lg bg-white`}
       >
         <input 
-          className="border-0 outline-0 w-[180px] text-[#101828] overflow-x-scroll" placeholder={props.placeholder} type={props.type} value={props.value}
+          className={`border-0 outline-0 text-[#101828] overflow-x-scroll`}
+          style={{width: `${props.width || "180px"}`}}
+          placeholder={props.placeholder}
+          type={props.type}
+          value={props.value}
           onChange={(e) => {props.onChange(e)}}
           onFocus={() => {setActive(true)}}
           onBlur={() => {setActive(false)}}
@@ -25,6 +29,11 @@ export default function Input(props) {
         )}
         {props.receiverSelect && (
           <ReceiverSelect setType={props.setReceiverType}/>
+        )}
+        {props.sideLabel && (
+          <div className="flex items-center cursor-pointer" >
+            <div className="text-[#101828] mr-1">{props.sideLabel}</div>
+          </div>
         )}
       </div>
     </div>
